c527aa348ae30259e2164577f712132d0a23203c
Mateusz Turbański <matrixxon@gmail.com>
2025-09-18 16:47:30 +0200

    Ready Template with tailwind

---
+++ ReactTemplate/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import "tailwindcss";
+
+function App() {
+  return (
+    <>
+      <div className="flex">
+        <div className="w-1/2 bg-amber-500">w-1/2</div>
+        <div className="w-1/2 bg-amber-900">w-1/2</div>
+      </div>
+    </>
+  )
+}
+
+export default App
